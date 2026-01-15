6ff3ceae6212742a7ff3988444b80f9069e95c48
gaston10com <enzothome1@gmail.com>
2026-01-15 16:03:01 -0300

    chore: analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import 'leaflet/dist/leaflet.css'
 import 'leaflet-draw/dist/leaflet.draw.css'
 import './index.css'
 import { App } from './App'
+import { inject } from "@vercel/analytics";
+
+inject();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
